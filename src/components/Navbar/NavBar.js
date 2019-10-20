3628f8521a1c59154c08e08e0b2ba0c1fdf06533
Atayi <atayisika@gmail.com>
2019-10-20 17:45:30 +0200

    Insertion du logo SNCF Transilien dans le composant 'NavBar'

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './NavBar.css';
+import logo from '@sncf/bootstrap-sncf.metier/src/assets/img/brand/logo_transilien.png';
 
 function NavBar() {
   return (
-    <header role="banner" className="container mastheader-title d-xl-block">
-      <nav className="NavBar">
-        <div className="mastheader">Logo</div>
+    <header role="banner" className="navbar">
+      <nav className="container-fluid bg-light d-flex justify-content-center justify-content-lg-start">
+        <div className="mastheader-logo"><img alt='Logo SNCF Transilien' src={logo}/></div>
       </nav>
     </header>
   );
