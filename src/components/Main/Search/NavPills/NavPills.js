1df0b49f7b747de9bb6f5fa8bdeb26e0bb85407c
Atayi <atayisika@gmail.com>
2019-10-23 12:41:17 +0200

    Modification du composant NavPills

--- src/components/Main/Search/NavPills/NavPills.js
+++ src/components/Main/Search/NavPills/NavPills.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './NavPills.css';
 
 function NavPills() {
     return(
         <div className='row'>
-            <div className='col-12'>
-                <ul class="nav nav-pills row mb-3" id="pills-tab" role="tablist">
+            <div className='col-sm-12'>
+                <ul class="nav nav-pills row trans-tabs nav access-tabs mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item col-md-4 col-lg-3">
                         <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
                             Itinéraires
@@ -16,9 +17,13 @@ function NavPills() {
                         </a>
                     </li>
                 </ul>
-                <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+                <div class="tab-content py-3 mx-2" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-home form-itineraire" role="tabpanel" aria-labelledby="pills-home-tab">
+                        form...
+                    </div>
+                    <div class="tab-pane fade next-departures" id="pills-profile form-temps-reel" role="tabpanel" aria-labelledby="pills-profile-tab">
+                        form...
+                    </div>
                 </div>
             </div>
         </div>
