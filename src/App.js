67d958b92fef54d0db12ab8929493743fec807e2
Atayi <atayisika@gmail.com>
2019-10-24 20:51:47 +0200

    Creation des fichiers Footer.css, Footer.js du composant Footer et importation du composant Footer dans le fichier App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf.css";
 import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf.js";
 import NavBar from "./components/Header/Navbar/NavBar";
 import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Main />
+      <Footer />
     </div>
   );
 }
