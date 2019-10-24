ea85bb1cdb5815571cb47f8fc4a04281fb728213
Armougom <y.armougom@gmail.com>
2019-10-24 20:51:41 +0200

    changement boostrap version min

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf.css";
-import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf.js";
+import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf.min.css";
+import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf.min.js";
 import NavBar from "./components/Header/Navbar/NavBar";
-import Main from './components/Main/Main';
+import Main from "./components/Main/Main";
 
 function App() {
   return (
