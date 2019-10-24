6ec7b0de002d470e47248f404576e2d4e69ebed8
Amel ALEM <mlle.amel@hotmail.fr>
2019-10-24 15:53:57 +0200

    integration banniere

---
+++ src/components/Main/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+    return(
+    <div className="container banner-scnf d-none d-lg-block">
+        <div className="banner ">
+            <a href="https://www.transilien.com/fr/acheter-mon-forfait-navigo-en-ligne"></a>
+        </div>  
+    </div>      
+    )
+}
+
+export default Banner;
