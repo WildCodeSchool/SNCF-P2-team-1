e2baf8f2cf7c8049844eedcd73cc51e4fffbdf39
Atayi <atayisika@gmail.com>
2019-10-24 20:17:12 +0200

    Modification de Main.css, Search.css et Search.js pour mettre un margin top au dessus de la zone de recherche de la page d'acceuil et éviter que les boutons 'Itinéraires' et 'Prochains départs' soit cachés derrière le header en responsive

--- src/components/Main/Search/Search.js
+++ src/components/Main/Search/Search.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Search.css';
 import NavPills from './NavPills/NavPills';
 import Journey from './Journey/Journey';
 import Departure from './Departure/Departure';
 
 function Search() {
     return(
-        <div className='container search mt-5 pt-5'>
+        <div className='container search'>
             <NavPills />
             <div className="tab-content py-3 row" id="pills-tabContent">
                 <Journey />
