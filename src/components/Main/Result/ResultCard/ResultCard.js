b982d1afefb3539aa524880bd71a2e32fe5f11f5
Atayi <atayisika@gmail.com>
2019-10-30 11:21:55 +0100

    Modification de l'affichage des heures et durée d'un trajet dans ResultCard

--- src/components/Main/Result/ResultCard/ResultCard.js
+++ src/components/Main/Result/ResultCard/ResultCard.js
@@ -13,54 +13,53 @@ const [result ] = useState(sampleResult);
     <div className='container'>
         <div className='row'>
             <div className='col-12 result-card'>
-                {result.journeys.map(journey =>
-                <ul key={journey.dateArrival}>
-                    <li>
-                        <ul className='result-time'>
-                            <li>
-                                {journey.dateArrival.slice(journey.dateArrival.indexOf('T')+1)}
-                            </li>
-                            <li>
-                                {journey.dateDeparture.slice(journey.dateArrival.indexOf('T')+1)}
-                            </li>
-                        </ul>
-                        <ul className='result-duration'>
-                            <li>
-                                {journey.totalDuration.replace("PT", "").toLowerCase()}
-                            </li>
-                            <li className='walkingDuration'>
-                                {journey.walkingDuration.replace("PT", "").toLowerCase()}
-                                <p>de marche</p>
-                            </li>
-                        </ul>
-                        
-                    </li>
-                    <li className='result-roadmap'>
-                        {journey.sections.map(
-                            transport => {
-                                if(transport.transport) {
-                                    //console.log(transport.transport)
-                                    if(transport.transport.line) {
-                                        return (
-                                        <span key={transport.arrival.dateTime}> {transport.transport.mode} {transport.transport.line.label}</span>
-                                        )
+                {result.journeys.map((journey,index) => { 
+                    const timeArrival = journey.dateArrival.slice(journey.dateArrival.indexOf('T')+1);
+                    const timeDeparture = journey.dateDeparture.slice(journey.dateDeparture.indexOf('T')+1);
+                    const timeDuration = journey.totalDuration.replace("PT", "").toLowerCase();
+                    const timeWalkingDuration = journey.walkingDuration.replace("PT", "").toLowerCase();
+                    return(
+                    <ul key={index}>
+                        <li>
+                            <ul className='result-time'>
+                                <li>{timeArrival.replace(timeArrival.slice(timeArrival.length-3), '')}</li>
+                                <li>{timeDeparture.replace(timeDeparture.slice(timeDeparture.length-3), '')}</li>
+                            </ul>
+                            <ul className='result-duration'>
+                                <li>{timeDuration.replace('m', ' min ')}</li>
+                                <li className='walkingDuration'>
+                                    dont {timeWalkingDuration.replace('m', ' min ')}
+                                    <p>de marche</p>
+                                </li>
+                            </ul>
+                            
+                        </li>
+                        <li className='result-roadmap'>
+                            {journey.sections.map(
+                                (transport, index) => {
+                                    if(transport.transport) {
+                                        //console.log(transport.transport)
+                                        if(transport.transport.line) {
+                                            return (
+                                            <span key={index}> {transport.transport.mode} {transport.transport.line.label}</span>
+                                            )
+                                        }
+                                        if(transport.transport.mode === 'WALKING'){
+                                            return(<span className='icon' key={index}> <FontAwesomeIcon icon={faWalking}/> </span>)
+                                        }
+                                        return(<span key={index}>{transport.transport.mode}</span>)
                                     }
-                                    if(transport.transport.mode === 'WALKING'){
-                                        return(<span className='icon'> <FontAwesomeIcon icon={faWalking}/> </span>)
-                                    }
-                                    return(<span key={transport.arrival.dateTime}>{transport.transport.mode}</span>)
                                 }
-                            }
-                        )}
-                    </li>
-                    <li>
-                        <ul>
-                            <li className='result-price'>{journey.price ? journey.price / 100 + '0 €':  '-'}</li>
-                            <li className='result-details'>Détails</li>
-                        </ul>
-                    </li>
-                </ul>
-                )}
+                            )}
+                        </li>
+                        <li>
+                            <ul>
+                                <li className='result-price'>{journey.price ? journey.price / 100 + '0 €':  '-'}</li>
+                                <li className='result-details'>Détails</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    )})}
             </div>
         </div>
     </div>
