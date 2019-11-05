f7fc59eff96460ed62483f334291c64504c13787
Armougom <y.armougom@gmail.com>
2019-11-05 23:42:17 +0100

    Ajout react-router, affichage detailCard au clic sur "details"

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import './Main.css';
-import Search from './Search/Search';
-import Banner from './Banner/Banner';
-import Result from './Result/Result';
-import DetaiCard from './Result/DetailCard/DetaiCard';
+import React from "react";
+import "./Main.css";
+import Search from "./Search/Search";
+import Banner from "./Banner/Banner";
+import Result from "./Result/Result";
+import DetailCard from "./Result/DetailCard/DetailCard";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function Main() {
   return (
     <main>
-      <Banner />
-      <Search />
-      <Result />
-      <DetaiCard />
+      <BrowserRouter>
+        <Banner />
+        <Search />
+        <Switch>
+          <Route exact path="/" component={Result} />
+          <Route path="/details:index" component={DetailCard} />
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
