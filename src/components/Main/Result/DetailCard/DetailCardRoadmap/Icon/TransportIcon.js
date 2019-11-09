9befcd437c0ab01ebf156e3952906cf2a3a5788b
Atayi <atayisika@gmail.com>
2019-11-09 10:17:59 +0100

    Creation du composant TransportIcon et du dossier d'image des icons pour afficher l'image de l'icon sur la page de détail d'un résultat

--- src/components/Main/Result/DetailCard/DetailCardRoadmap/Icon/TransportIcon.js
+++ src/components/Main/Result/DetailCard/DetailCardRoadmap/Icon/TransportIcon.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {faTrain, faBus} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import RER from '../img/RER.svg';
+import TRAM from '../img/TRAM.png';
+import METRO from '../img/Metro.svg.png';
 
 const fontAwesomeIcons = {
   TRAIN: faTrain,
@@ -11,11 +14,26 @@ const fontAwesomeIcons = {
 };
 
 function TransportIcon(props) {
-  return (
-    <i>
-      <FontAwesomeIcon icon={fontAwesomeIcons[props.icon]} />
-    </i>
-  );
+  switch (props.icon) {
+    case 'TRAIN':
+      return (
+        <i>
+          <FontAwesomeIcon icon={fontAwesomeIcons[props.icon]} />
+        </i>
+      );
+    case 'RER':
+      return <img src={RER} alt="RER" />;
+    case 'METRO':
+      return <img src={METRO} alt="METRO" />;
+    case 'TRAM':
+      return <img src={TRAM} alt="TRAM" />;
+    default:
+      return (
+        <i>
+          <FontAwesomeIcon icon={fontAwesomeIcons[props.icon]} />
+        </i>
+      );
+  }
 }
 
 export default TransportIcon;
