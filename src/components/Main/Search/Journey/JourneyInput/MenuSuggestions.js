04bea50908c597d628c6a36ae2ec38f3e6f0ac0b
Armougom <y.armougom@gmail.com>
2019-11-11 00:11:54 +0100

    Ajout message d'erreur si input invalide (placeholder et alert)

--- src/components/Main/Search/Journey/JourneyInput/MenuSuggestions.js
+++ src/components/Main/Search/Journey/JourneyInput/MenuSuggestions.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-const MenuSuggestions = () => {
+const MenuSuggestions = ({ suggestions, suggestionsSelected, setActive }) => {
+  const refUl = useRef();
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("click", handleClick2);
+    return () => {
+      document.removeEventListener("click", handleClick2);
+    };
+  });
+  const handleClick2 = e => {
+    if (!refUl.current.contains(e.target)) {
+      setActive();
+    }
+  };
+
   return (
-    <ul className="UlSuggestions" ref={this.myRef}>
+    <ul className={"UlSuggestions"} ref={refUl}>
       {suggestions.map((x, index) => (
         <li
           className="LiSuggestions"
           key={index}
-          onClick={() => this.suggestionsSelected(x)}
+          onClick={() => suggestionsSelected(x)}
         >
           {x.name}
         </li>
