0d8edc7e9c922d860116e36a86d17f93c1a881ed
Mohammed-Sebaa <mmohamedsebaa@gmail.com>
2019-11-12 11:02:20 +0100

    Espace avant le signe euro enlevé

--- src/components/Main/Result/DetailCard/DetailCardRoadmap/DetailCardRoadmap.js
+++ src/components/Main/Result/DetailCard/DetailCardRoadmap/DetailCardRoadmap.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import _get from 'lodash/get';
-import Moment from 'react-moment';
-import {useSelector} from 'react-redux';
-import {useHistory} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faWalking, faClock} from '@fortawesome/free-solid-svg-icons';
-import './DetailCardRoadmap.css';
-import TransportIcon from './Icon/TransportIcon';
+import React from "react";
+import _get from "lodash/get";
+import Moment from "react-moment";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWalking, faClock } from "@fortawesome/free-solid-svg-icons";
+import "./DetailCardRoadmap.css";
+import TransportIcon from "./Icon/TransportIcon";
 
-function DetailCardRoadmap({match}) {
+function DetailCardRoadmap({ match }) {
   let history = useHistory();
   //Data Itineraire
   const data = useSelector(
@@ -18,7 +18,7 @@ function DetailCardRoadmap({match}) {
   );
   //retourne a l'acceuil si aucune données reçus
   if (!data) {
-    history.push('/');
+    history.push("/");
     return null;
   }
   const dataDetails = data.sections;
@@ -31,37 +31,37 @@ function DetailCardRoadmap({match}) {
             <FontAwesomeIcon icon={faClock} />
           </i>
           <strong>
-            {' '}
+            {" "}
             {data.totalDuration
-              .slice(2, data.totalDuration.indexOf('M'))
+              .slice(2, data.totalDuration.indexOf("M"))
               .toLowerCase()
-              .replace('h', ' h ')}{' '}
-            min{' '}
+              .replace("h", " h ")}{" "}
+            min{" "}
           </strong>
           <span>
-            {' '}
-            dont{' '}
+            {" "}
+            dont{" "}
             <i>
               <FontAwesomeIcon icon={faWalking} />
-            </i>{' '}
+            </i>{" "}
             {data.walkingDuration
               .toLowerCase()
-              .slice(2, data.walkingDuration.indexOf('M'))}{' '}
+              .slice(2, data.walkingDuration.indexOf("M"))}{" "}
             min
           </span>
           <div className="my-2 zoneTarif">
-            Zone {data.zones ? data.zones.min : ' '} -{' '}
-            {data.zones ? data.zones.max : ' '} | Tarifs :{' '}
-            {data.price ? data.price / 100 + ' €' : ' - '}
+            Zone {data.zones ? data.zones.min : " "} -{" "}
+            {data.zones ? data.zones.max : " "} | Tarifs :{" "}
+            {data.price ? data.price / 100 + "€" : " - "}
           </div>
           {dataDetails
-            .filter(detail => _get(detail, 'departure', false))
+            .filter(detail => _get(detail, "departure", false))
             .map((detail, index) => {
               const color = detail.color;
               const transportColor = {
                 backgroundColor: `${color}`,
-                margin: '1em',
-                padding: '.5em',
+                margin: "1em",
+                padding: ".5em"
               };
               const iconTransport = detail.transport.mode;
               return (
@@ -76,7 +76,7 @@ function DetailCardRoadmap({match}) {
                       {detail.totalDuration
                         .slice(2, detail.totalDuration.length - 1)
                         .toLowerCase()
-                        .replace('h', ' h ')}{' '}
+                        .replace("h", " h ")}{" "}
                       min
                     </li>
                     <li>
@@ -89,14 +89,14 @@ function DetailCardRoadmap({match}) {
                     </li>
                     <li
                       className={
-                        detail.transport.mode === 'WALKING' ? 'dotted' : ''
+                        detail.transport.mode === "WALKING" ? "dotted" : ""
                       }
                     ></li>
                     <li>
-                      {detail.transport.mode === 'WALKING' ? (
+                      {detail.transport.mode === "WALKING" ? (
                         <i className="walking"></i>
                       ) : (
-                        ''
+                        ""
                       )}
                       <span className="whiteCircle"></span>
                     </li>
@@ -107,10 +107,10 @@ function DetailCardRoadmap({match}) {
                       <span className="direction">dir. {detail.direction}</span>
                     </li>
                     <li>
-                      {detail.transport.mode === 'WALKING' ? (
+                      {detail.transport.mode === "WALKING" ? (
                         <FontAwesomeIcon icon={faWalking} />
                       ) : (
-                        ''
+                        ""
                       )}
                     </li>
                     <li>{detail.arrival.label}</li>
