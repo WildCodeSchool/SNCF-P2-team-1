795a1cc020d91e32d45c1b3909fd1125d8a5d739
Armougom <y.armougom@gmail.com>
2019-11-13 15:01:37 +0100

    Ajout Scrolltop (page details affiché en haut)

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './Main.css';
-import Search from './Search/Search';
-import Banner from './Banner/Banner';
-import Result from './Result/Result';
-import DetailCard from './Result/DetailCard/DetailCard';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import "./Main.css";
+import Search from "./Search/Search";
+import Banner from "./Banner/Banner";
+import Result from "./Result/Result";
+import DetailCard from "./Result/DetailCard/DetailCard";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ScrollToTop from "../../ScrolltoTop";
 
 function Main() {
   return (
@@ -13,7 +14,11 @@ function Main() {
         <Route exact path="/" component={Banner} />
         <Route exact path="/" component={Search} />
         <Route exact path="/" component={Result} />
-        <Route exact path="/details:index" component={DetailCard} />
+
+        <ScrollToTop />
+        <Switch>
+          <Route exact path="/details:index" component={DetailCard} />
+        </Switch>
       </BrowserRouter>
     </main>
   );
